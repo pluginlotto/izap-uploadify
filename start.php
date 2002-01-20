29ab30663b9790e89cfefbb4c7b8917a3e4f1b5d
Carl <carl@izap.in>
2002-01-20 20:35:41 +0000

    New 1.8

---
+++ start.php
@@ -0,0 +1,79 @@
+<?php
+/**************************************************
+* iZAP Web Solutions				  *
+* Copyrights (c) 2005-2009. iZAP Web Solutions.	  *
+* All rights reserved				  *
+***************************************************
+* @author iZAP Team "<support@izap.in>"
+* @link http://www.izap.in/
+* Under this agreement, No one has rights to sell this script further.
+* For more information. Contact "Tarun Kumar<tarun@izap.in>"
+ */
+
+elgg_register_event_handler('init', 'system', 'init_izap_uploadify');
+define('GLOBAL_IZAP_UPLOADIFY_PLUGIN', 'izap-uploadify');
+
+
+function init_izap_uploadify() {
+  global $CONFIG;
+
+  // temp upload folder
+
+  $CONFIG->izapUploadify['uploadFolder'] = $CONFIG->dataroot;
+
+  // lets include the javascript files
+  elgg_extend_view('css/screen', 'izap-uploadify/css');
+
+  // lets register some actions
+  elgg_register_action('izap-uploadify/upload', $CONFIG->pluginspath . GLOBAL_IZAP_UPLOADIFY_PLUGIN . '/actions/izap-uploader.php');
+elgg_register_js('izap-uploadify','mod/' . GLOBAL_IZAP_UPLOADIFY_PLUGIN . '/vendors/jquery.progress.js');
+
+//c($CONFIG);
+}
+
+
+
+function izapGetUploadedFiles_izap_uploadify() {
+  $uploadedFiles = $_SESSION['uploadFolder'];
+  unset($_SESSION['uploadFolder']);
+
+  return $uploadedFiles;
+}
+
+function izap_get_progress_bar_settings($name) {
+  $value = elgg_get_plugin_setting($name, GLOBAL_IZAP_UPLOADIFY_PLUGIN);
+
+  switch ($name) {
+    case 'progressbar_background_color':
+      if(empty($value) || $value === FALSE) {
+        $value = 'FFC536';
+      }
+      $value = '#' . $value;
+      break;
+
+    case 'progressbar_foreground_color':
+      if(empty($value) || $value === FALSE) {
+        $value = 'D9ECFF';
+      }
+      $value = '#' . $value;
+      break;
+
+    case 'progressbar_height':
+      if(empty($value) || $value === FALSE) {
+        $value = '15px';
+      }
+      break;
+
+    case 'progressbar_width':
+      if(empty($value) || $value === FALSE) {
+        $value = '100%';
+      }
+      break;
+
+    default:
+      return FALSE;
+      break;
+  }
+
+  return $value;
+}
